d1c35fabdc269fdf541a039dad5ee40c67fe9a16
Hen3DL <delgadohenry77@gmail.com>
2024-05-05 17:13:03 -0700

    Add files via upload
    Instruction Memory

---
+++ InstructionMemory.v
@@ -0,0 +1,11 @@
+module InstructionMemory #(parameter MEM_SIZE = 256) (data_out, address_in, clk);
+  output reg [31:0] data_out;
+  input [31:0] address_in;
+  input clk;
+
+  reg [31:0] Internal_mem [0:MEM_SIZE-1];
+
+  always @(posedge clk) begin
+    data_out <= Internal_mem[address_in];
+  end
+endmodule
