b733e4e845e08645c53a4307522ecfb203206ac7
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-05-10 21:03:52 -0700

    Update ID_EX_TB.v
    Fixed Some errors

--- ID_EX_TB.v
+++ ID_EX_TB.v
@@ -5,10 +5,11 @@ module ID_EX_tb;
 // Signals
 reg clk;
 reg RegWrite_in, MemtoReg_in, MemRead_in, MemWrite_in, RegDst_in, ALU_Src_in;
+reg [1:0] ALU_Op_in;
 reg [31:0] Read_Data_1_in, Read_Data_2_in, sign_extend_in;
 reg [4:0] IF_ID_Rs_in, IF_ID_Rt_in, ID_Rd_in;
-wire RegWrite_out, MemtoReg_out, MemRead_out, MemWrite_out, RegDst_out,, ALU_Src_out;
-wire [1:0] ALU_Op_out, ALU_Op_in;
+wire RegWrite_out, MemtoReg_out, MemRead_out, MemWrite_out, RegDst_out, ALU_Src_out;
+wire [1:0] ALU_Op_out;
 wire [31:0] Read_Data_1_out, Read_Data_2_out, sign_extend_out;
 wire [4:0] ID_EX_Rs_Forward_out, ID_EX_Rt_Forward_out, ID_EX_Rs_MUX_out, ID_EX_Rt_MUX_out;
 
@@ -54,7 +55,7 @@ end
 initial begin
     clk = 0;
     Read_Data_1_in = 32'b1010;// Wire in makes it so that wire out should be 1010
-    
+    ALU_Op_in = 2'b10;
 end
 
 // Stimulus
