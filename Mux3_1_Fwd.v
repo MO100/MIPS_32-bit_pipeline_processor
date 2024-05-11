aac208d10a73eeb2a6d4d7b3987c5cc2254c6572
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-05-10 19:59:30 -0700

    Update Mux3_1_Fwd.v
    Changed it with respect to signals from forwarding unit

--- Mux3_1_Fwd.v
+++ Mux3_1_Fwd.v
@@ -1,10 +1,17 @@
-module Mux3_1_Fwd(input[31:0] A_in, B_in, C_in, input[2:0] select, output reg [31:0] data_out);
-always @(*)begin
-    case(select)
-	3'b000: data_out = A_in;
-	3'b001: data_out = B_in;
-	3'b010: data_out = C_in;
-	default: data_out = 5'b00000;
-     endcase
-  end
+module Mux3_1_Fwd(Mux3_1_Fwd_out, Register_Data_in, EX_MEM_result_in, MEM_WB_result_in, Forward_signal_in);
+output reg [31:0] Mux3_1_Fwd_out;
+//Three inputs based on what type of hazard and where we need to forward from either EX/MEM (R type hazard) or MEM/WB (LW/SW Hazard) or no hazard at all (Register_data_in)
+input [31:0] Register_Data_in, EX_MEM_result_in, MEM_WB_result_in;
+input [1:0] Forward_signal_in;
+
+always@(*) begin
+//Made the case statements here
+  case(Forward_signal_in)
+    2'b00: Mux3_1_Fwd_out <= Register_Data_in;
+    2'b01: Mux3_1_Fwd_out <= MEM_WB_result_in;
+    2'b10: Mux3_1_Fwd_out <= EX_MEM_result_in;
+  default: Mux3_1_Fwd_out <= 32'b10101010; //for testing error 
+endcase
+end 
+
 endmodule
