3ff094a72846b4a2c84ecbffb63dcc4c821a98c9
MO100 <39598174+MO100@users.noreply.github.com>
2024-05-11 10:34:51 -0700

    Update Data_Memory.v
    Syntax error on for loop

--- Data_Memory.v
+++ Data_Memory.v
@@ -11,8 +11,9 @@ module Data_Memory(data_out, address_in, write_data_in, mem_write_in, mem_read, 
 	
 	always @ (posedge clk) begin
 		if (reset)
-			for (i = 0; i < 1023; i = i + 1)
+			for (i = 0; i < 1023; i = i + 1) begin
 				memory_internal[i] <= 0;
+			end
 		else if (mem_write_in)
 				{memory_internal[base_address], memory_internal[base_address + 1], memory_internal[base_address + 2], memory_internal[base_address + 3]} <= write_data_in;
 	end
@@ -31,6 +32,32 @@ module Data_Memory(data_out, address_in, write_data_in, mem_write_in, mem_read, 
 	end
 
 endmodule
+
+/*
+	output [31:0] data_out;
+	input [31:0] data_in, address_in;
+	input write_in, clk;
+
+	reg [7:0] Memory_internal [0:1023];
+
+	integer i;
+
+	// Initialize memory with zeros
+	initial begin
+		for (i = 0; i < 1023; i = i + 1) begin
+		  Memory_internal[i] = 32'h00000000;
+		end
+	end
+
+	always @(posedge clk) begin
+		if (write_in == 1'b1) begin
+		  Memory_internal[address_in] <= data_in;
+		end
+	end
+
+	assign data_out = Memory_internal[address_in];
+	//data_out will be a concatination of four bites
+	//data_out = {}*/
 /*module Data_Memory(data_out, data_in, address_in, write_in, clk);
   output [31:0] data_out;
   input [31:0] data_in, address_in;
