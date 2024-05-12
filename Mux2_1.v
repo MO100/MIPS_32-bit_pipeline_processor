d4a04d9c309497a17fab6669fccf7b93bc0723e4
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-05-11 19:10:49 -0700

    Update Mux2_1.v

--- Mux2_1.v
+++ Mux2_1.v
@@ -1,5 +1,8 @@
-module Mux2_1(input[31:0] A_in, B_in, input[1:0] select, output reg [31:0] data_out);
-always @(*)begin
-    data_out <= select ? A_in : B_in;
+module Mux2_1(A_in, B_in, select, data_out);
+input[31:0] A_in, B_in;
+input select;
+output reg [31:0] data_out;
+always @(*)begin    
+data_out <= select ? B_in : A_in; //if select = 1, then data_out = B_in, else vice versa
   end
 endmodule
