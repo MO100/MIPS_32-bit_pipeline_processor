e95e206801de6577a967839d2eeb928a932d211d
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-05-13 21:41:30 -0700

    Update InstructionMemory.v

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,4 +1,4 @@
-module InstructionMemory (data_out, address_in, clk);
+module InstructionMemory #(parameter FILE = "")(data_out, address_in, clk);
   parameter MEM_SIZE = 256;
   output reg [31:0] data_out;
   input [31:0] address_in;
@@ -6,6 +6,10 @@ module InstructionMemory (data_out, address_in, clk);
 
   reg [31:0] Internal_mem [0:MEM_SIZE-1];
 
+  initial begin
+    $readmemh(FILE, memory_internal);
+  end
+  
   always @(posedge clk) begin
     data_out <= Internal_mem[address_in];
   end
